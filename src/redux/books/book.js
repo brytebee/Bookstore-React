773fd698087f615a80312390a53f50facabd845b
brytebee <brytebee@gmail.com>
2021-12-14 12:44:51 +0100

    :white_check_mark: Add getBook function
    Add getBook function to handle getting book Action

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,5 +1,8 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookStore/books/GET_BOOK';
+const baseUrl =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0MxoaYAlLWZ6BQnrAqvb/books/';
 
 const initialState = [];
 
@@ -11,6 +14,11 @@ export const addBook = (payload) => ({
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
+});
+
+export const getBook = (allBooks) => ({
+  type: GET_BOOK,
+  payload: allBooks,
 });
 
 const bookReducer = (state = initialState, action) => {
