7f6f20596b46e69874b659cfd09b577972fab5ad
brytebee <brytebee@gmail.com>
2021-12-14 10:39:34 +0100

    :package: Install UUID
    Install UUID for unique id and update form

--- src/components/books/form.js
+++ src/components/books/form.js
@@ -1,15 +1,41 @@
 import { Button } from '@material-ui/core';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+      category,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+    setCategory('Choose category');
+  };
+
   return (
     <form className="d-flex j-spa">
       <input
         type="text"
         className="title form-input"
         placeholder="Book title"
+        onClick={(e) => setTitle(e.target.value)}
         required
       />
-      <select name="category" className="form-input" required>
+      <select
+        name="category"
+        className="form-input"
+        onClick={(e) => setCategory(e.target.value)}
+        required
+      >
         <option defaultValue="Choose category">Choose category</option>
         <option value="Movies">Movies</option>
         <option value="Music">Music</option>
@@ -19,7 +45,12 @@ export default function Form() {
         <option value="Finance">Finance</option>
         <option value="Personal Development">Personal Development</option>
       </select>
-      <Button variant="contained" className="submit" color="primary">
+      <Button
+        variant="contained"
+        className="submit"
+        color="primary"
+        onSubmit={submitBookToStore}
+      >
         Add book
       </Button>
     </form>
