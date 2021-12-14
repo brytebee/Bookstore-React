92d2a8292c338199ebb36b92d239301e79ac32b5
brytebee <brytebee@gmail.com>
2021-12-14 13:38:10 +0100

    :white_check_mark: Update Action
    Update action directory

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,6 +1,6 @@
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const GET_BOOK = 'bookStore/books/GET_BOOK';
+const ADD_BOOK = 'bookStore-react/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore-react/books/REMOVE_BOOK';
+const GET_BOOK = 'bookStore-react/books/GET_BOOK';
 const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0MxoaYAlLWZ6BQnrAqvb/books/';
 
 const initialState = [];
