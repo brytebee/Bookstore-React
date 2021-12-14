cc8e500edd423bd04832ca2fad1fb8328a3493a9
brytebee <brytebee@gmail.com>
2021-12-14 12:42:15 +0100

    :white_check_mark: Update dataflow
    Setup bookCard to receive data from API

--- src/components/books/bookCard.js
+++ src/components/books/bookCard.js
@@ -1,4 +1,5 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { generateProgress } from '../../dommyData';
 import BookInfo from './bookInfo';
 import Chapter from './chapter';
@@ -6,7 +7,11 @@ import Bookprogress from './progress';
 import '../../App.css';
 
 export default function BookCard() {
+  const dispatch = useDispatch();
   const allBooks = useSelector((state) => state.bookReducer);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
   return (
     <div>
       {allBooks.map((book) => (
